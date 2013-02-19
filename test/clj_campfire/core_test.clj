dad1e2041035d9fef421da328d9ce8102d85de21
Max Gonzih <gonzih@gmail.com>
2013-02-19 16:45:26 +0300

    Add descriptions for facts

--- test/clj_campfire/core_test.clj
+++ test/clj_campfire/core_test.clj
@@ -2,12 +2,10 @@
   (:require [midje.sweet :refer :all]
             [clj-campfire.core :refer :all]))
 
-(fact (stream-url "13")
-      =>
+(fact (stream-url "13") =>
       "https://streaming.campfirenow.com/room/13/live.json")
 
-(fact (api-url "mydomain" "/data.json")
-      =>
+(fact (api-url "mydomain" "/data.json") =>
       "https://mydomain.campfirenow.com/data.json")
 
 (fact "auth map generation"
@@ -19,20 +17,21 @@
     (:password   (auth stub-token))       => "x"
     (:preemptive (auth stub-token false)) => false))
 
-(fact (let [data "some data"]
-        (callback "some state" data) => [data :continue]
-        (provided (process-data data) => anything :times 1)))
+(fact "callback should pass data to process-data function"
+  (let [data "some data"]
+    (callback "some state" data) => [data :continue]
+    (provided (process-data data) => anything :times 1)))
 
 (let [raw-data "{\"room_id\":3,\"body\":\"pur\"}\r
                 {\"room_id\":5,\"body\":\"yes\"}\r"
       parsed-data [{"room_id" 3 "body" "pur"}
                    {"room_id" 5 "body" "yes"}]]
-  (fact
+  (fact "process-data should call process-message for each message in data"
     (process-data raw-data) => anything
     (provided (parse-data raw-data) => parsed-data :times 1
               (process-message
                 (as-checker #(some #{%} parsed-data)))
               => anything :times 2))
 
-  (fact
+  (fact "parse-data should return collection of parsed messages"
     (parse-data raw-data) => parsed-data))
